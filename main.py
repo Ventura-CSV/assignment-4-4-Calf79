c2b7b159cfa641f824541db38ba3c55ebfe1840b
Calf79 <camerino_alfaro2@my.vcccd.edu>
2024-06-29 23:47:47 -0700

    Assignemnt 4-4 Camerino

--- main.py
+++ main.py
@@ -6,7 +6,16 @@ def main():
     Code Your Program here
     ########################################
     """
-
+    for i in range(5):
+        num= int(input(f'enter value:'))
+        numbers.append(num)
+    minval=numbers[0]
+    maxval=numbers[0]
+    for num in numbers:
+        if num<minval:
+            minval=num
+        if num>maxval:
+            maxval=num
     print(*numbers)
     print(maxval, minval)
     ########################################
